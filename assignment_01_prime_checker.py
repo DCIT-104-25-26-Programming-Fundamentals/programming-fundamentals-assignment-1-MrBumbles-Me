c2163d4193c6f21e9fe69e94265761c56711a813
MrBumbles-Me <aymansaani@gmail.com>
2026-07-27 16:40:43 +0000

    i was re checking the code to enable it run

--- assignment_01_prime_checker.py
+++ assignment_01_prime_checker.py
@@ -1,16 +1,18 @@
-def is_prime(n):
-    """Return True if n is a prime number, False otherwise."""
-    if n < 2:
+def is_prime(number):
+    if number < 2:
         return False
-    for divisor in range(2, int(n ** 0.5) + 1):
-        if n % divisor == 0:
+
+    for i in range(2, int(number ** 0.5) + 1):
+        if number % i == 0:
             return False
+
     return True
 
 
-if __name__ == "__main__":
-    num = int(input("Enter a number: "))
-    if is_prime(num):
-        print(f"{num} is a prime number.")
-    else:
-        print(f"{num} is NOT a prime number.")
+# Main program block
+num = int(input("Enter a number: "))
+
+if is_prime(num):
+    print(f"{num} is a prime number.")
+else:
+    print(f"{num} is NOT a prime number.")
